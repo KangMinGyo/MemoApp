9ab828f89cc3cabe3d1762f6154c710b9b505c12
KangMingyo <kang1@KangMingyoui-MacBookPro.local>
2022-05-27 22:30:17 +0900

    First commit

---
+++ MgMemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MgMemo
+//
+//  Created by KangMingyo on 2022/05/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
